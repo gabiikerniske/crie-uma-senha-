d06b07e721fa768d2d480ed8b4cbea55f482a7b1
gabiikerniske <112017069+gabiikerniske@users.noreply.github.com>
2024-05-29 11:35:54 -0300

    2905

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ botoes[1].onclick = aumentaTamanho;
 function diminuiTamanho(){
     if(tamanhoSenha > 1){
     //tamanhoSenha = tamanhoSenha - 1;
+    tamanhoSenha--;
     }
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
@@ -15,14 +16,32 @@ function diminuiTamanho(){
 function aumentaTamanho(){
     if(tamanhoSenha < 20){
     //tamanhoSenha = tamanhoSenha + 1;
+    tamanhoSenha++;
     }
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
 }
+
 // codigo omitido
 const campoSenha = document.querySelector('#campo-senha');
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
 function geraSenha(){
+    let alfabeto = '';
+    if (checkbox[0].checked){
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+    if (checkbox[1].checkbox){
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+    if (checkbox[2].checkbox){
+        alfabeto = alfabeto + numeros;
+    }
+    if (checkbox[3].checkbox){
+        alfabeto = alfabeto + simbolos;
+    }
     let senha = '';
     for (let i = 0; i < tamanhoSenha; i++){
     let numeroAleatorio = Math.random()*letrasMaiusculas.length;
